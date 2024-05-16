bbfaa9e848e378e617e5cbc7e21a48e809850e54
Julia Ortiz Mora <jortiz-m@c3r2s1.42malaga.com>
2024-05-16 11:30:41 +0200

    primero

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,21 @@
 /*   By: jortiz-m <jortiz-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:30:46 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/05/15 10:31:37 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/05/16 11:29:57 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <fcntl.h>
+
 char	*get_next_line(int fd)
 {
-	
+	int	fb;
+
+	fd = open(O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	read()
+	close(fd);
+	return (0);
 }
