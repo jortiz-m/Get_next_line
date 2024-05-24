1b583eb7d2ed45e0b2fc42b4e726a815187552b6
Julia Ortiz Mora <jortiz-m@c3r3s2.42malaga.com>
2024-05-24 12:47:29 +0200

    tres

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,34 @@
 /*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:30:46 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/05/24 10:07:57 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/05/24 12:45:25 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
-char	*ft_stash(buffer[i]) //lee caracter y lo almacena temporalmente
+
+char	*ft_reserve(char *buffer)
 {
-	char	*temp;
-	int		i;
+	int		count;
+	char	*str;
 
-	i = 0;
-	temp = malloc(BUFFER_SIZE * sizeof(char));
-	while (buffer[i] != '\0' && buffer[i] != '\n')
+	count = 0;
+	while ((buffer[count] != '\n' || buffer[count] != '\0') \
+	&& count < BUFFER_SIZE)
+		count++;
+	str = malloc((count + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	count = 0;
+	while (buffer[count] != '\0')
 	{
-		temp[i] = buffer[i];
-		i++;
+		str[count] = buffer[count];
+		count++;
 	}
-	return (&temp);
-}
-
-char	*ft_reserve(ft_stash) //crea el str completo, en cada vuelta añade
-{
-	char	*str; //mi str final
-	int		i;
-
-	str = malloc(); //que memoria le doy?? en cada vuelta es mayor
-	i = 0;
-	while (str[i] == '\0') //colocarme en el último elemento del string
-		i++;
-	while(temp[i] '\0')
-	{
-		str[i] = temp[i];
-		i++;
-	}
+	str[count] = '\0';
 	return (str);
 }
 
-void	ft_freestash(char *str)
-{
-	free(temp);
-}
-*/
 char	*get_next_line(int fd)
 {
 	char	*str;
@@ -59,24 +44,24 @@ char	*get_next_line(int fd)
 	i = 0;
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	element = read(fd, buffer, BUFFER_SIZE);
-	if(element != 0)
-	{
-		while (buffer[i] != '\0')
-		{
-			ft_stash(&buffer[i]);
-			i++;
-		}
-	}
-	else
-		return(-1);
-	return(buffer);
+	if (element < 0)
+		return (NULL);
+	while (i < BUFFER_SIZE)
+		i++;
+	buffer[i] = '\0';
+	str = ft_reserve(buffer);
+	free(buffer);
+	return (str);
 }
-int main()
+
+int	main(void)
 {
-	int fd;
-	char *line;
+	int		fd;
+	char	*line;
 
 	fd = open("caca.txt", O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s\n", line);
 	line = get_next_line(fd);
 	printf("%s\n", line);
 	return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,20 @@
 /*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 10:52:27 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/05/23 11:21:22 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/05/24 12:46:45 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 28
 
+# include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd);
 
 #endif
