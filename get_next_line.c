7659cdd12d54ccaf0d4638741ef5aa0128488042
Julia Ortiz Mora <jortiz-m@c3r3s2.42malaga.com>
2024-05-24 10:12:47 +0200

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,59 +6,78 @@
 /*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:30:46 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/05/23 11:27:05 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/05/24 10:07:57 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-/*char	*ft_stash(fd) //lee caracter y lo devuelve
+/*
+char	*ft_stash(buffer[i]) //lee caracter y lo almacena temporalmente
 {
-	char 	*temp;
+	char	*temp;
 	int		i;
 
 	i = 0;
-	temp = malloc(2 * sizeof(char));
-	read(fd, &temp, 1);
+	temp = malloc(BUFFER_SIZE * sizeof(char));
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+	{
+		temp[i] = buffer[i];
+		i++;
+	}
 	return (&temp);
-}*/
+}
 
-char *ft_reserve(char c) //crea el str completo, en cada vuelta añade
+char	*ft_reserve(ft_stash) //crea el str completo, en cada vuelta añade
 {
 	char	*str; //mi str final
 	int		i;
 
-	str = malloc()
+	str = malloc(); //que memoria le doy?? en cada vuelta es mayor
 	i = 0;
 	while (str[i] == '\0') //colocarme en el último elemento del string
 		i++;
-	str[i] = temp;
-	i++;	
+	while(temp[i] '\0')
+	{
+		str[i] = temp[i];
+		i++;
+	}
+	return (str);
 }
 
-void	ft_freestash(char *str) //liberar la memoria en cada vuelta
+void	ft_freestash(char *str)
 {
+	free(temp);
 }
-
+*/
 char	*get_next_line(int fd)
 {
-	char	*str; //string final
+	char	*str;
 	char	*buffer;
 	int		element;
 	int		i;
 
 	i = 0;
-	element = read(fd, buffer, BUFFER_SIZE)
-	if (element != 0) //lee y devuelve un número el read
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	element = read(fd, buffer, BUFFER_SIZE);
+	if(element != 0)
 	{
 		while (buffer[i] != '\0')
 		{
-			ft_reserve(buffer[i]); //crear el srting final que devolvemos
+			ft_stash(&buffer[i]);
 			i++;
 		}
-	ft_freestash() //liberamos el buffer para la siguiente lectura
 	}
 	else
-		return (-1);
-	return (ft_reserve);
+		return(-1);
+	return(buffer);
+}
+int main()
+{
+	int fd;
+	char *line;
+
+	fd = open("caca.txt", O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s\n", line);
+	return (0);
 }
