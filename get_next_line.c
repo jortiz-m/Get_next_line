da539070ff6e5d4ece6fa96606cb1cd37baa11f8
Julia Ortiz Mora <jortiz-m@c3r2s1.42malaga.com>
2024-05-27 13:39:30 +0200

    cuatro

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,75 @@
 /*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:30:46 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/05/24 12:45:25 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/05/27 13:38:31 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_reserve(char *buffer)
+size_t	ft_len(const char *str)
 {
-	int		count;
-	char	*str;
+	size_t	c;
 
-	count = 0;
-	while ((buffer[count] != '\n' || buffer[count] != '\0') \
-	&& count < BUFFER_SIZE)
-		count++;
-	str = malloc((count + 1) * sizeof(char));
+	c = 0;
+	while (str[c] != '\0' || str[c] != '\n')
+		c++;
+	return (c);
+}
+
+char	*ft_rest(char *buffer, int element)
+{
+	char	*rest;
+	int		start;
+
+	rest = malloc((element - ft_len(buffer) * sizeof(char)));
+	if (rest == NULL)
+		return (NULL);
+	start = buffer[ft_len(buffer) + 2];
+	while (start < element)
+	{
+		rest[start] = buffer[start];
+		start++;
+	}
+	return (rest);
+}
+
+char	*ft_createstr(char *temp, size_t len)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	count = 0;
-	while (buffer[count] != '\0')
+	while (i > len)
 	{
-		str[count] = buffer[count];
-		count++;
+		str[i] = temp[i];
+		i++;
 	}
-	str[count] = '\0';
 	return (str);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*str;
-	char	*buffer;
-	int		element;
-	int		i;
+	char		*str;
+	char		*buffer;
+	int			element;
+	int			i;
+	char		*rest;
 
 	i = 0;
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buffer == NULL)
+		return (NULL);
 	element = read(fd, buffer, BUFFER_SIZE);
 	if (element < 0)
 		return (NULL);
-	while (i < BUFFER_SIZE)
-		i++;
-	buffer[i] = '\0';
-	str = ft_reserve(buffer);
+	if (ft_len(buffer) < element)
+		rest = ft_rest(buffer, element);
+		strjoin(str rest)
+	str = ft_createstr(buffer, element);
 	free(buffer);
 	return (str);
 }
