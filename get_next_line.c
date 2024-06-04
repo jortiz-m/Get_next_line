34b0ef0729a51cc14a3793b13ad005c2ee870d89
Julia Ortiz Mora <jortiz-m@c3r2s1.42malaga.com>
2024-06-04 12:04:41 +0200

    siete

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:30:46 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/05/31 15:48:16 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/06/04 12:03:40 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,26 @@ int	ft_strlen(char *str)
 {
 	int	counter;
 
+	if (str == NULL)
+		return(0);
 	counter = 0;
 	while (str[counter] != '\0')
 		counter++;
 	return (counter);
+}
+
+int ft_findstr(const char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
 int	ft_check(char *buffer)
@@ -59,11 +75,11 @@ char	*ft_read(int fd)
 	char	*buffer;
 	int		element;
 
-	buffer = malloc(BUFFER_SIZE * sizeof(char));
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if	(!buffer)
 		return (NULL);
 	element = read(fd, buffer, BUFFER_SIZE);
-	buffer[BUFFER_SIZE] = '\0';
+	buffer[element] = '\0';
 	return (buffer);
 }
 
@@ -73,66 +89,81 @@ char	*ft_rest(char *buffer)
 	int		j;
 	char	*rest;
 
-	i = 0;
-	while (buffer[i] != '\n')
-		i++;
-	i++;
+	i = ft_findstr(buffer, '\n');
 	j = 0;
-	while (buffer[i] != '\0')
+	while (buffer[j] != '\0')
 		j++;
-	rest = malloc((j + 1) * sizeof(char));
-	if (!rest)
-		return (NULL);
-	j = 0;
-	while (buffer[i] != '\0')
+	while (j > i)
 	{
-		rest[j] = buffer[i + j];
-		j++;
+		rest = malloc((j + 1) * sizeof(char));
+		if (!rest)
+			return (NULL);
+		j = 0;
+		while (buffer[i] != '\0')
+		{
+			rest[j] = buffer[i + j];
+			j++;
+		}
+		rest[j] = '\0';
+		free(buffer);
+	return (rest);	
 	}
-	rest[j] = '\0';
-	free(buffer);
-	return (rest);
+	
 }
 
 char	*ft_join(char *buffer, char *new)
 {
-	char	*newbuffer;
+	char	*newbuffer = NULL;
 	int		i;
 	int		j;
 
 	i = 0;
-	newbuffer = malloc(ft_strlen(buffer) + ft_strlen(new) * sizeof(char));
-	while (buffer[i])
+	newbuffer = (char *)malloc((ft_strlen(buffer) + ft_strlen(new) + 1) * sizeof(char));
+	if (!newbuffer)
+		return (NULL);
+	if (buffer)
 	{
-		newbuffer[i] = buffer[i];
-		i++;
+		while (buffer[i])
+			newbuffer[i] = buffer[i++];
 	}
 	j = 0;
-	while (new)
+	if (new)
 	{
-		newbuffer[i + j] = new[j];
-		j++;
+		while (new[j] != '\0')
+			newbuffer[i + j] = new[j++];
 	}
 	free(buffer);
 	free(new);
+	newbuffer[i + j] = '\0';
 	return (newbuffer);
 }
 char	*get_next_line(int fd)
 {
-	static char *buffer;
+	static char *buffer = NULL;
 	char		*line;
+	char		*temp;
 
-	buffer = ft_read(fd);
-	while (ft_check(buffer) == 0)
-		buffer = ft_join(buffer, ft_read(fd));
-		if (ft_null(buffer) == 1)
-			
-	if (ft_check(buffer) == 1)
+	
+	buffer = ft_join(buffer, ft_read(fd));
+	if (ft_strlen(buffer) == BUFFER_SIZE)
 	{
-		line = ft_createline(buffer);
-		buffer = ft_rest(buffer);
+		while (ft_check(buffer) == 0)
+		{
+			temp = ft_join(temp, buffer);
+			free(buffer);
+			buffer = temp;
+		}
+		if (ft_check(buffer) == 1)
+		{
+			line = ft_createline(buffer);
+			temp = ft_rest(buffer);
+			free(buffer);
+			buffer = temp;
+			return (line);	
+		}
 	}
-	return (line);
+	else
+		return(buffer);
 }
 
 int	main(void)
